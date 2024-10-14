265c70655757625fe7dc0b31b5a5f3e4805762ca
alexrsanderson <46336731+alexrsanderson@users.noreply.github.com>
2024-10-14 16:11:55 -0400

    Battle works again
    Minimum viable product. v1.0
    Avg wr: 13%
    -Review both td est and target so they don't return the same Q value but the shape is fine
    -Metrics

--- EliteTrainer.py
+++ EliteTrainer.py
@@ -43,13 +43,13 @@ class EliteTrainer(EnvPlayer, GenData):
         self.gamma = 0.9
         self.batch_size = 32
         self.device = 'cpu'
-        self.net = PokeNet(self.observation_space, self.action_space.n).float()
+        self.net = PokeNet(self.observation_space.shape, self.action_space.n)
         self.memory = TensorDictReplayBuffer(storage=LazyMemmapStorage(100000, device=torch.device("cpu")))
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr = 0.00025)
-        self.loss_fn = torch.nn.SmoothL1Loss
+        self.loss_fn = torch.nn.SmoothL1Loss()
         self.exploration_rate = 1
-        self.decay = 0.25
-        self.explore_min = 0.05
+        self.decay = 0.01
+        self.explore_min = 0.01
         self.curr_step = 0
         self.burnin = 1
         self.learn_every = 1
@@ -57,23 +57,22 @@ class EliteTrainer(EnvPlayer, GenData):
         self.action_id = 0
     def calc_reward(self, last_battle: AbstractBattle, current_battle: AbstractBattle) -> float:
         return self.reward_computing_helper(
-            current_battle, fainted_value=0.1, hp_value=0, victory_value=1
+            current_battle, fainted_value=0.5, hp_value=0.1, victory_value=3
         )
-    
-    def action_to_move(self, action: int, battle: AbstractBattle) -> BattleOrder:
+    def optimal_move(self, state) -> int:
         if np.random.rand() < self.exploration_rate:
             action_idx = np.random.randint(26)
-            self.action_id = action_idx
         else:
             state = state[0].__array__() if isinstance(state, tuple) else state.__array__()
-            state = torch.tensor(state, device=torch.device('cpu')).unsqueeze(0)
+            state = torch.tensor(state, device=self.device).unsqueeze(0)            
             action_values = self.net(state, model='online')
-            action_idx = torch.argmax(action_values, axis=1).item()
+            action_idx = torch.argmax(action_values, dim=1).item()
         self.exploration_rate *= self.decay
         self.exploration_rate = max(self.explore_min, self.exploration_rate)
         self.curr_step += 1
         self.action_id = action_idx
-        return Gen9EnvSinglePlayer.action_to_move(self, action_idx, battle=self.current_battle)
+    def action_to_move(self, action: int, battle: AbstractBattle) -> BattleOrder:
+        return Gen9EnvSinglePlayer.action_to_move(self, self.action_id, battle)
     def embed_battle(self, battle: AbstractBattle) -> ObsType:
         # -1 indicates that the move does not have a base power
         # or is not available
@@ -114,23 +113,23 @@ class EliteTrainer(EnvPlayer, GenData):
             np.array(high, dtype=np.float32),
             dtype=np.float32,
         )
-    def cache(self, state, next_state, action, reward, done):
+    def cache(self, state, next_state, action, reward, terminated):
         def first_if_tuple(x):
             return x[0] if isinstance(x, tuple) else x
         state = first_if_tuple(state).__array__()
         next_state = first_if_tuple(next_state).__array__()
         state = torch.tensor(state)
         next_state = torch.tensor(next_state)
-        action = torch.tensor([action])
-        reward = torch.tensor([reward])
-        done = torch.tensor([done])
+        action = torch.tensor(action)
+        reward = torch.tensor(reward)
+        terminated = torch.tensor(terminated)
         self.memory.add(TensorDict({'state': state, 'next_state':next_state, 
-        'action':action, 'reward':reward, 'done':done},batch_size=[]))
+        'action':action, 'reward':reward, 'terminated':terminated},batch_size=[]))
     def recall(self):
         batch = self.memory.sample(self.batch_size).to(self.device)
-        state, next_state, action, reward, done = (batch.get(key) for key in ('state', 'next_state', 'action',
-        'reward', 'done'))
-        return state, next_state, action.unsqueeze(), reward.squeeze(), done.squeeze
+        state, next_state, action, reward, terminated = (batch.get(key) for key in ('state', 'next_state', 'action',
+        'reward', 'terminated'))
+        return state, next_state, action, reward, terminated
     def learn(self):
         if self.curr_step % self.sync_every == 0:
             self.sync_Q_target()
@@ -138,27 +137,24 @@ class EliteTrainer(EnvPlayer, GenData):
             return None, None
         if self.curr_step % self.learn_every !=0:
             return None, None
-        state, next_state, action, reward, done = self.recall()
+        state, next_state, action, reward, terminated = self.recall()
         td_est = self.td_estimate(state, action)
-        td_tgt = self.td_target(reward, next_state, done)
+        td_tgt = self.td_target(reward, next_state, terminated)
         loss = self.update_Q_online(td_est, td_tgt)
         return (td_est.mean().item(),loss)
     def td_estimate(self, state, action):
-        current_Q = self.net(state, model='online')[
-            np.arange(0, ), action
-        ]
+        current_Q = self.net(state, model='online').gather(1, action.view(self.batch_size, 1))
         return current_Q
+    @torch.no_grad()
     def td_target(self, reward, next_state, done):
         next_state_Q = self.net(next_state, model = 'online')
-        optimal_move = torch.argmax(next_state_Q, axis=1)
-        next_Q = self.net(next_state, model='target')[
-            np.arange(0, self.batch_size), optimal_move
-        ]
-        return (reward + (1 - done.float()) * self.gamma * next_Q).float()
+        optimal_move = torch.argmax(next_state_Q, dim=1, keepdim=True)
+        next_Q_values = self.net(next_state, model='target').gather(1, optimal_move)
+        return (reward.view(self.batch_size, 1) + (1 - done.float()).view(self.batch_size, 1) * self.gamma * next_Q_values)
     def update_Q_online(self, td_estimate, td_target):
         loss = self.loss_fn(td_estimate, td_target)
         self.optimizer.zero_grad()
-        loss.backward()
+        loss.backward(retain_graph=True)
         self.optimizer.step()
         return loss.item()
     def sync_Q_target(self):
@@ -167,14 +163,14 @@ class PokeNet(nn.Module):
     def __init__(self, input, output):
         super(PokeNet, self).__init__()
         output_size = int(output)
-        input_size = int(np.prod(input.shape))
+        input_size = int(np.prod(input))
         self.online = self._build_dqn(input_size, output_size)
         self.target = self._build_dqn(input_size, output_size)
         self.target.load_state_dict(self.online.state_dict())
         for p in self.target.parameters():
             p.requires_grad = False
     def forward(self, input, model):
-        if model == 'online':
+        if model == 'online': 
             return self.online(input)
         elif model == 'target':
             return self.target(input)
@@ -182,6 +178,7 @@ class PokeNet(nn.Module):
         return nn.Sequential(
             nn.Linear(input,64),
             nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(64,64),
             nn.ReLU(),
             nn.Linear(64, output_size)
@@ -199,16 +196,21 @@ async def main():
         for ep in range(episodes):
             state = env.reset()
             while True:
-                action = env.action_to_move(state, env.current_battle)  
-                obs, reward, done, truncated, info = env.step(action)
-                env.cache(state, obs, env.action_id, reward, done)
+                #print(type(state))
+                env.optimal_move(state)
+                #print(f"Optimal move (integer): {optimal_move} (type: {type(optimal_move)})")
+                action = env.action_to_move(env.action_id, env.current_battle)  
+                next_state, reward, terminated, truncated, info = env.step(action)
+                #print(f"Action to move: {action} (type: {type(action)})")
+                env.cache(state, next_state, env.action_id, reward, terminated)
                 q, loss = env.learn()
-                state = obs
+                state = next_state
                 env.render()
-                if done or truncated:
+                if terminated or truncated:
                     break
         print(
         f"Double DQN Evaluation: {env.n_won_battles} victories out of {env.n_finished_battles} episodes")
+        train_env.reset_env(restart=True)
         train_env.close(purge=True)
     n_episodes = 50
     run(train_env, n_episodes)
